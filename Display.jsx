90ad78d47ce72c85b04fdc8732ca05c051d8076e
Sumit kumar <152763704+SumitKumarsk001@users.noreply.github.com>
2024-08-07 20:14:46 +0530

    Add files via upload
    React-Js React-components

---
+++ Display.jsx
@@ -0,0 +1,5 @@
+import styles from "./Display.module.css";
+const Display =({displayValue})=>{
+  return <input type="text" value={displayValue} readOnly className={styles.display} />
+}
+export default Display;
